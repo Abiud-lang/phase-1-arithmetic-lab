5d492510d95eed0610d239d9bcfac7ef860af8bc
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-06-25 14:17:38 +0300

    change arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+ 
+
+const  num1 = 31
+const  num2 = 2
+
+const multiply = num1 * num2
+
+
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+
+if (random === 0) {
+  random = 1; 
+}
+
+console.log(random);
+
+
+let num3 = 1; 
+let num4 = 0;  
+
+
+let mod = (num4 * 3) + 4;
+
+console.log(mod); 
+``
+let max = 20;
+
+console.log(max);
