1d229b060aa1e8dba09f3f4ccf8496b6183bdadb
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-06-29 00:38:34 +0300

    changed random

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ const  num2 = 2
 const multiply = num1 * num2
 
 
-let random = Math.ceil(Math.random() * 1);
+let random = 1
 
 
 if (random === 0) {
   random = 1; 
 }
-
-console.log(random);
 
 
 let num3 = 1; 
@@ -24,8 +22,7 @@ let num4 = 0;
 
 let mod = (num4 * 3) + 4;
 
-console.log(mod); 
+
 ``
 let max = 20;
 
-console.log(max);
